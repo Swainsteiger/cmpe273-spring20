c878ff8ff7be9243e0b0027f9c18aaa8acb243c3
Swainsteiger <54923715+Swainsteiger@users.noreply.github.com>
2020-03-04 22:14:11 -0800

    Add files via upload

--- assignment1/tcp/tcp_client.py
+++ assignment1/tcp/tcp_client.py
@@ -1,23 +1,32 @@
 import socket
-
+import sys
+import time
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5000
 BUFFER_SIZE = 1024
 MESSAGE = "ping"
 
-def send(id=0):
+
+def send(id):
+    delay = int(sys.argv[2])
+    repeat_num = int(sys.argv[3])
+    count = 1
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
-    s.send(f"{id}:{MESSAGE}".encode())
-    data = s.recv(BUFFER_SIZE)
+    while count <= repeat_num:
+        s.send(f"{id}:{MESSAGE}".encode())
+        data = s.recv(BUFFER_SIZE)
+        print("received data:", data.decode())
+        count += 1
+        time.sleep(delay)
     s.close()
-    print("received data:", data.decode())
 
 
 def get_client_id():
-    id = input("Enter client id:")
+    id = sys.argv[1]
     return id
 
 
-send(get_client_id())
+if __name__ == "__main__":
+    send(get_client_id())
--- assignment1/tcp/tcp_server.py
+++ assignment1/tcp/tcp_server.py
@@ -1,26 +1,33 @@
 import socket
-
+from threading import Thread
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5000
 BUFFER_SIZE = 1024
 
+
 def listen_forever():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((TCP_IP, TCP_PORT))
-    s.listen(1)
+    s.listen(10)
 
-    conn, addr = s.accept()
-    print(f'Connection address:{addr}')
+    print(f'Server at port:{TCP_PORT}')
 
     while True:
+        conn, addr = s.accept()
+        Thread(target=client_thread, args=(conn, addr)).start()
+
+
+def client_thread(conn,addr):
+    while True:
         data = conn.recv(BUFFER_SIZE)
-        if not data: 
-            print('No data received.')
+        if not data:
+            print(f'End of data transmission from client[IP:{addr[0]},Port:{addr[1]}]')
+            conn.close()
             break
         print(f"received data:{data.decode()}")
         conn.send("pong".encode())
 
-    conn.close()
 
-listen_forever()
+if __name__ == "__main__":
+    listen_forever()
